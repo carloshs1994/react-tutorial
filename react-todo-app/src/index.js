da167c94e1e427238dfa756cf6543156d4a311b3
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-02-14 10:58:15 -0600

    Add required files for the project

---
+++ react-todo-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoContainer from './components/TodoContainer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
